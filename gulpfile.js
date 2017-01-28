1506ccfe2c47c467d9a0c29c36ba7746d76d480f
Wessel Grift <info@baasdesign.nl>
2017-01-28 23:10:57 +0100

    Fix typography line height

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,61 @@
 'use strict';
 
-var gulp              = require('gulp'),
-    sass              = require('gulp-sass'),
-    autoprefixer      = require('gulp-autoprefixer'),
-    sourcemaps        = require('gulp-sourcemaps'),
-    cssmin            = require('gulp-cssmin'),
-    rename            = require('gulp-rename'),
-    del               = require('del'),
-    taskTime          = require('gulp-total-task-time'),
-    stripCssComments  = require('gulp-strip-css-comments'),
-    cssbeautify       = require('gulp-cssbeautify'),
-    header            = require('gulp-header'),
-    pkg               = require('./package.json'),
-    watch             = require('gulp-watch');
+var gulp             = require('gulp'),
+    sass             = require('gulp-sass'),
+    autoprefixer     = require('gulp-autoprefixer'),
+    sourcemaps       = require('gulp-sourcemaps'),
+    cssmin           = require('gulp-cssmin'),
+    rename           = require('gulp-rename'),
+    del              = require('del'),
+    taskTime         = require('gulp-total-task-time'),
+    stripCssComments = require('gulp-strip-css-comments'),
+    cssbeautify      = require('gulp-cssbeautify'),
+    header           = require('gulp-header'),
+    pkg              = require('./package.json'),
+    watch            = require('gulp-watch');
 
 /* Header banner */
 var banner = ['/**',
-            '  * Pavilion CSS Framework',
-            '  * Version: <%= pkg.version %>',
-            '  * Website: <%= pkg.homepage %>',
-            '  * License: <%= pkg.license %>',
-            '  */',
-            '\n'].join('\n');
+        '  * Pavilion CSS Framework',
+        '  * Version: <%= pkg.version %>',
+        '  * Website: <%= pkg.homepage %>',
+        '  * License: <%= pkg.license %>',
+        '  */',
+        '\n'
+    ].join('\n');
 
 /* Record total task time */
 taskTime.init();
 
-/* Clean 'dist' and 'demo/css' folders */
+/* Clean task - cleans'dist' and 'demo/css' folders */
 gulp.task('clean', function() {
-    return del(['dist/*','demo/css/*']);
+    return del(['dist/*', 'demo/css/*']);
 });
 
 /* Compile SCSS, Autoprefix, Stripcomments, Beautify, Minify. */
 gulp.task('scss-dist', function() {
     return gulp.src('scss/pavilion.scss')
         .pipe(sourcemaps.init())
-        .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
+        .pipe(sass({ 
+            outputStyle: 'expanded' 
+        }).on('error', sass.logError))
+        .pipe(autoprefixer({ 
+            browsers: ['last 2 versions'], 
+            cascade: false 
         }))
-        .pipe(stripCssComments({
-            preserve: false
+        .pipe(stripCssComments({ 
+            preserve: false 
         }))
         .pipe(cssbeautify())
-        .pipe(header(banner, { pkg : pkg }))
+        .pipe(header(banner, { 
+            pkg: pkg 
+        }))
         .pipe(gulp.dest('dist/'))
         .pipe(gulp.dest('demo/css'))
         .pipe(cssmin())
-        .pipe(rename({ suffix: '.min' }))
+        .pipe(rename({ 
+            suffix: '.min' 
+        }))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('dist/'));
 });
@@ -58,6 +65,8 @@ gulp.task('watchscss', function() {
     gulp.watch('scss/**/*', ['clean', 'scss-dist']);
 });
 
+/* Watch task */
 gulp.task('watch', ['clean', 'scss-dist', 'watchscss']);
 
+/* Default task */
 gulp.task('default', ['clean', 'scss-dist']);
